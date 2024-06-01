88cf03643b2f17dfe968363378c2f266e5c780dd
FranticDreamer <49658961+edgarbarney@users.noreply.github.com>
2024-06-01 14:50:29 +0300

    Fix Indentation in fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -9,28 +9,28 @@ author "FranticDreamer"
 ui_page 'html/index.html'
 
 client_script {
-  "client/client.lua",
+	"client/client.lua",
 }
 server_script {
-  "server/server.lua",
-  "@oxmysql/lib/MySQL.lua",
+	"server/server.lua",
+	"@oxmysql/lib/MySQL.lua",
 }
 
 shared_script {
-  "config.lua",
+	"config.lua",
 }
 
 files {
 	'html/index.html',
 	'html/js/*.js',
-  'html/js/plugins/*.js',
-  'html/css/*.css',
+	'html/js/plugins/*.js',
+	'html/css/*.css',
 }
 
 dependencies {
-  'oxmysql',
+	'oxmysql',
 }
 
 --escrow_ignore {
---  'Config.lua',
+--	'Config.lua',
 --}
